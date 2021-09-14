6956cfe27bee8fe201c6d9f3a499f638916a49d5
Şevval Özlem ÇARKIT <63058707+svvlcrkt@users.noreply.github.com>
2021-09-14 09:39:48 +0300

    Add files via upload

---
+++ withJs/script.js
@@ -0,0 +1,118 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+// console.log(items);
+let futureDate = new Date(2021, 8, 16, 17, 30, 0);
+const year = futureDate.getFullYear();
+// console.log(year);
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+let month = months[futureDate.getMonth()];
+// console.log(month);
+
+let weekDay = weekdays[futureDate.getDay()];
+// console.log(weekDay);
+
+const date = futureDate.getDate();
+
+
+ 
+giveaway.textContent = `giveaway ends on ${weekDay}, ${date} ${month} ${year} ${hours}:${mins}`;
+
+// future time in ms
+const futureTime = futureDate. getTime();
+// getTime() method returns the number of miliseconds 
+// console.log(futureTime); gives 1631543245197
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    // console.log(today);
+    const t = futureTime - today;
+    // console.log(t);
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60min
+    // 1d = 24hr
+
+    // values in ms
+
+    // how many ms in one day?
+    const oneDay = 24*60*60*1000;
+    // console.log(oneDay);
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+    // calculate all values
+    let days = t/oneDay;
+    // we don't want to see the decimal part
+    // so we use Math.floor method
+    days = Math.floor(days);
+    console.log(days);
+
+    // we need to look at the remainder of the number of days
+    // the mod will give us the rest
+    let hours = (t%oneDay)/oneHour;
+    hours = Math.floor(hours);
+    console.log(hours);
+
+    // then we took the mode of the oneHour to find the rest
+    let minutes = (t%oneHour)/oneMinute;
+    minutes = Math.floor(minutes);
+    console.log(minutes);
+
+    let seconds = Math.floor((t%oneMinute)/1000);
+
+    // set values array
+    const values = [days, hours, minutes, seconds];
+
+
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`;
+        }
+        return item;
+    }
+
+    items.forEach(function(item, index){
+        item.innerHTML = format(values[index]);
+    })
+
+    // if current time greater than future time, then there will be a problem
+    // We can solve this problem by writing a text on the screen.
+    if(t<0){  
+        clearInterval(countdown);  // clearInterval method cancels the call that was previously created with setInterval
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+
+    }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime,1000);
+getRemainingTime();
+
+
---
+++ withjQuery/script.js
@@ -0,0 +1,86 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const giveaway = $('.giveaway');
+const deadline = $('.deadline');
+const items = $('.deadline-format h4');
+
+let futureDate = new Date(2021, 8, 16, 17, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const date = futureDate.getDate();
+
+let month = months[futureDate.getMonth()];
+let weekDay = weekdays[futureDate.getDay()];
+
+giveaway.text(`giveaway ends on ${weekDay}, ${date} ${month} ${year} ${hours}:${mins}`);
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+
+    const oneDay = 24*60*60*1000;
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+
+    let days = t/oneDay;
+    days = Math.floor(days);
+    let hours = (t%oneDay)/oneHour;
+    hours = Math.floor(hours);
+
+    let minutes = (t%oneHour)/oneMinute;
+    minutes = Math.floor(minutes);
+
+    let seconds = (t%oneMinute)/1000;
+    seconds = Math.floor(seconds);
+
+    const values = [days, hours, minutes, seconds];
+
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`;
+        }
+        return item;
+    }
+
+    items.each(function(index, item){
+        // console.log("74");
+        $(item).text(format(values[index]));
+    })
+
+    if(t<0){
+        clearInterval(countdown);
+        deadline.text(`<h4 class="expired">sorry, this giveaway
+        has expired</h4>`)
+    }
+
+}
+
+let countdown = setInterval(getRemainingTime,1000);
+getRemainingTime();
